26faf3f27fded540e1a3ffb633d582dd2509ebc8
DELL-PC <DELL-PC@DESKTOP-A1KRGJM.fios-router.home>
2017-04-03 22:51:26 -0400

    first commit

---
+++ src/SeleniumCertificationProject/sauceconnect.java
@@ -0,0 +1,14 @@
+package SeleniumCertificationProject;
+
+public class sauceconnect {
+
+	
+		// TODO Auto-generated method stub
+	public static final String USERNAME = "prems1985";
+	  public static final String ACCESS_KEY = "46512760-760a-4331-8521-f9f9747fccdc";
+	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	
+	
+	
+
+}
